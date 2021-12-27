cc6c0646a155c92645b3956b89eb47689eab5648
Joseph Burton <burtonjae@hotmail.co.uk>
2021-12-27 14:23:47 +0000

    Repackage

--- src/lib.rs
+++ src/lib.rs
@@ -12,16 +12,16 @@ fn clock() -> &'static Clock {
 }
 
 #[no_mangle]
-pub extern "system" fn Java_test_AccurateTimer_initialize(_env: JNIEnv, _class: JClass) {
+pub extern "system" fn Java_accuratetimer_AccurateTimer_initialize(_env: JNIEnv, _class: JClass) {
     clock(); // calibrate
 }
 
 #[no_mangle]
-pub extern "system" fn Java_test_AccurateTimer_rdtsc(_env: JNIEnv, _class: JClass) -> jlong {
+pub extern "system" fn Java_accuratetimer_AccurateTimer_rdtsc(_env: JNIEnv, _class: JClass) -> jlong {
     clock().raw() as jlong
 }
 
 #[no_mangle]
-pub extern "system" fn Java_test_AccurateTimer_delta(_env: JNIEnv, _class: JClass, start: jlong, end: jlong) -> jlong {
+pub extern "system" fn Java_accuratetimer_AccurateTimer_delta(_env: JNIEnv, _class: JClass, start: jlong, end: jlong) -> jlong {
     clock().delta(start as u64, end as u64).as_nanos() as jlong
 }
